72ddff5c274f367d8948d0f18b10e6a3b3ef8da0
CptSoap <cowbear6598@gmail.com>
2020-12-12 16:32:41 +0800

    1.0.12

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 'use strict';
 
-module.exports = require('./soap.crypt');
-module.exports = require('./soap.date');
-module.exports = require('./soap.log');
-module.exports = require('./soap.message');
-module.exports = require('./soap.utils');
+const utils = require('./lib/soap.utils');
+
+exports.CheckDirectoryExist = utils.CheckDirectoryExist;
+
+const message = require('./lib/soap.message');
+
+exports.Packet = message.packet;
+
+const log = require('./lib/soap.log');
+
+exports.Log = log.Log;
+
+const date = require('./lib/soap.date');
+
+exports.unixTime = date.unixTime;
+exports.unixTimeMillisecond = date.unixTimeMillisecond;
+exports.toIsoStringTimeZone = date.toIsoStringTimeZone;
+exports.yyyymmdd = date.yyyymmdd;
+exports.yyyymmddhhmmss = date.yyyymmddhhmmss;
+exports.yyyymmddIsToday = date.yyyymmddIsToday;
+
+const crypt = require('./lib/soap.crypt');
+
+exports.RsaDecrypt = crypt.RsaDecrypt;
+exports.RsaEncrypt = crypt.RsaEncrypt;
+exports.RsaCreateKeyPair = crypt.RsaCreateKeyPair;
